2cd807970a81c9fa2bf83f45c7c29df12774e7aa
Francis John <francis.john@colorado.edu>
2015-01-24 15:46:38 -0800

    Add newrelic

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'activerecord', '~>4.1'
 gem 'rake'
 
 gem 'shotgun'
-
+gem 'newrelic_rpm'
 group :test do
   gem 'shoulda-matchers'
   gem 'rack-test'
--- config/environment.rb
+++ config/environment.rb
@@ -17,9 +17,8 @@ require 'logger'
 
 require 'sinatra'
 require "sinatra/reloader" if development?
-
 require 'erb'
-
+require 'newrelic_rpm'
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
